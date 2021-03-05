2e4cd715cf4556121b8540e2141f90fc36f2938a
WillianFarias <williansousafarias@gmail.com>
2021-03-05 18:31:49 -0300

    adicionando dependencia lombok ao pom e criando classe UserData

---
+++ src/main/java/com/avanade/dio/jwt/data/UserData.java
@@ -0,0 +1,13 @@
+package com.avanade.dio.jwt.data;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserData {
+  private String name;
+  private String password;
+}
