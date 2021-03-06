8a4577a98590131c00014e2ecd47e7f470326ed7
WillianFarias <williansousafarias@gmail.com>
2021-03-06 09:37:37 -0300

    AuthController

--- src/main/java/com/avanade/dio/jwt/security/WebSecurity.java
+++ src/main/java/com/avanade/dio/jwt/security/WebSecurity.java
@@ -6,18 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableWebFluxSecurity
+@EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
   
-  private static UserDetailsServiceImpl userDetailsService;
-  private static BCryptPasswordEncoder bCryptPasswordEncoder;
+  private final UserDetailsServiceImpl userDetailsService;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
   public WebSecurity(UserDetailsServiceImpl userDetailsService, 
   BCryptPasswordEncoder bCryptPasswordEncoder) {
@@ -33,7 +33,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
       .antMatchers(HttpMethod.GET, SecurityConstants.STATUS_URL)
       .permitAll()
       .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
-      .permitAll();
+      .permitAll()
+      .anyRequest().authenticated();
   }
 
   @Override
